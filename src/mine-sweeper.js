80f52c1b3adfae26e6f7cebb5f1529726789d13f
VanoKi <kirsvano@gmail.com>
2024-12-28 14:51:00 +0300

    feat: solve mineswiper finally

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(mine) {
+  const ans = JSON.parse(JSON.stringify(mine))
+  const sum = (array) => array.map(i => i === undefined ? 0 : i).reduce((acc, i) => acc + i)
+  const check = (item) => item !== undefined ? item : false
+  for (let i = 0; i < mine.length; i++) {
+    let row = check(mine[i])
+    let previousRow = check(mine[i - 1])
+    let nextRow = check(mine[i + 1])
+    for (let j = 0; j < row.length; j++) {
+      let previousRowSum = sum([previousRow[j - 1], previousRow[j], previousRow[j + 1]])
+      let currentRowSum = sum([row[j - 1], row[j + 1]])
+      let nextRowSum = sum([nextRow[j - 1], nextRow[j], nextRow[j + 1]])
+      ans[i][j] = sum([previousRowSum, currentRowSum, nextRowSum])
+    }
+  }
+  return ans
 }
 
 module.exports = {
