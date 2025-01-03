3181e04894b40bf70b60c0059d4c4351863b69ae
VanoKi <kirsvano@gmail.com>
2025-01-03 16:40:12 +0300

    fix: move common code to methods

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,7 +20,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  common(string, key, isEncrypt){
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.baseCharCode = 'a'.charCodeAt(0)
+  }
+
+  validate(...args) {
+    if (args.some(arg => arg === undefined || arg === null)) {
+      throw new Error('Incorrect arguments!')
+    }
+  }
+
+  common(string, key, operation){
     let ans = ''
     let baseCharCode = 'a'.charCodeAt(0)
     for (let i = 0; i < string.length; i++) {
@@ -30,16 +42,20 @@ class VigenereCipheringMachine {
       }
       let keyIndex = key[i % key.length].toLowerCase().charCodeAt(0) - baseCharCode
       let textIndex = string[i].toLowerCase().charCodeAt(0) - baseCharCode
-      let newIndex = isEncrypt ? ((textIndex + keyIndex) % 26) : (textIndex - keyIndex + 26) % 26
+      let newIndex = operation === 'encrypt'
+          ? ((textIndex + keyIndex) % 26)
+          : (textIndex - keyIndex + 26) % 26
       ans += String.fromCharCode(newIndex + baseCharCode).toUpperCase()
     }
-    return ans
+    return this.isDirect ? ans : ans.split('').reverse().join('')
   }
   encrypt(string, key) {
-    return this.common(string, key, true)
+    this.validate(string, key)
+    return this.common(string, key, 'encrypt')
   }
   decrypt(string, key) {
-    return this.common(string, key, false)
+    this.validate(string, key)
+    return this.common(string, key, 'decrypt')
   }
 }
 
