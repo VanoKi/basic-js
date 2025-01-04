5e42a4eb81ed3ae13ef6cabab5508d2ebb29cddb
VanoKi <kirsvano@gmail.com>
2025-01-04 15:37:24 +0300

    feat: first iteration of solve string repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let inner = str + Array.from({length:options.additionRepeatTimes})
+      .fill(options.addition)
+      .join(options.additionSeparator)
+  return Array.from({length:options.repeatTimes})
+      .fill(inner)
+      .join(options.separator)
 }
 
 module.exports = {
